05b20b013d1a788f5fad5cef5b42a4639b6d876c
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-04-07 23:32:24 +0530

    Added FullBlog

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -1,15 +1,49 @@
+import { memo } from "react";
+import { Blog } from "../pages/Blogs";
+import { Appbar } from "./Appbar";
+import Avatar from "./Avatar";
 
-const FullBlog = () => {
+const FullBlog = ({ blog }: { blog: Blog }) => {
+  const paragraphs: string[] = blog.content.split(".");
   return (
-    <div className='grid grid-cols-12 px-10'>
-        <div className='grid-cols-8'>
-            hii
-        </div>
-        <div className='grid-cols-4'>
-            hello 
-        </div>
-    </div>
-  )
-}
+    <>
+      <Appbar />
+      <div className="pt-12">
+        <div className="grid grid-cols-12 px-[12%] w-full max-w-screen-2xl">
+          <div className=" col-span-8">
+            <div className="md:text-5xl text-3xl w-full font-extrabold">
+              {blog.title}
+            </div>
+            <div className="text-slate-500 pt-2">Posted on 2nd dec 2023 </div>
 
-export default FullBlog
+            {paragraphs.map((content: string, index: number) => (
+              <div key={index} className="pt-4 w-full">
+                <br />
+                {content}
+              </div>
+            ))}
+          </div>
+          <div className="col-span-4">
+            <div className="text-slate-500 text-lg">Author</div>
+            <div className="flex w-full">
+              <div className="pr-2 flex flex-col justify-center">
+                <Avatar size={"12"} name={blog.author.name || "Anonymous"} />
+              </div>
+              <div>
+                <div className="text-xl font-bold">
+                  {blog.author.name || "Anonymous"}
+                </div>
+                <div className="pt-2 text-slate-500">
+                  Random catch pharse about the author's ability to grab the
+                  user's attention
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default memo(FullBlog);
