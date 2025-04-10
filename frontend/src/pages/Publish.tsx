466a13868f3a4a4eddf793a08910d2a80b5b09c6
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-04-10 23:25:48 +0530

    Update Publish

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { ChangeEvent, useState } from 'react'
 
 const Publish = () => {
+  const [description, setDescription] = useState("");
   return (
-    <div>Publish</div>
+    <div>
+      <TextEditor
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          />
+    </div>
   )
+}
+
+function TextEditor({
+  onChange,
+  value,
+}: {
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  value: string;
+}) {
+  return (
+    <>
+      <div>
+        <div className="w-full mb-4 border border-gray-300 rounded-xl ml-2.5">
+          <div className="flex items-center justify-between  border-gray-600">
+            {/* <div className="flex flex-wrap items-center sm:divide-x sm:rtl:divide-x-reverse  divide-gray-600"></div> */}
+          </div>
+
+          <div className="p-2 rounded-b-lg">
+            <label htmlFor="editor" className="sr-only">
+              Publish post
+            </label>
+            <textarea
+              value={value}
+              onChange={onChange}
+              id="editor"
+              rows={8}
+              className="block w-full px-0 text-sm focus:outline-none text-gray-800 bg-white  placeholder-gray-400"
+              placeholder="Write an article..."
+              required
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Publish
